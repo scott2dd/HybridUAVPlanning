f43209ffed5d7259125511ca2766a2b7b728a6d2
drew <you@example.com>
2023-02-24 11:35:58 -0500

    fixed type bugs fixed include / using order errors fixed build errors

--- src/TestPkg.jl
+++ src/TestPkg.jl
@@ -2,8 +2,11 @@
 
 module TestPkg
 
+incude("structs_def.jl")
 include("label_label_sel.jl")
 include("label_node_sel.jl")
+
+
 export hybrid_label_selection
 export hybrid_node_selection
 export EucGraphInt
@@ -54,10 +57,6 @@ export OptControlSolution
 export MILP_to_opt_ctrl
 
 
-
-
-
-
 import LinearAlgebra.norm
 using DataStructures
 using Graphs
@@ -73,13 +72,13 @@ using Gadfly
 using Cairo
 using Compose
 using GraphPlot
-using Statistics
+import Statistics.mean
 using Colors 
 using Interpolations
 using JuMP
 using CPLEX
-import Ipopt 
-import DifferentialEquations
+using Ipopt 
+using DifferentialEquations
 using Interpolations        
 
 
@@ -90,7 +89,6 @@ using Interpolations
 #and
 #export
 
-# 1) Data Structs
 # 2) Labeling Utils
 # 3) Labeling funcs (from labeling.jl)
 # 4) Plotting Functions
@@ -98,137 +96,8 @@ using Interpolations
 # 6) MILP Functions
 # 7) Optimal Control utils
 # 7) Merge Algo and Utils
-
 # 7) Merge testing....
-###########################################
-## 1: Data Structs
-struct ProblemDef
-    S::Int64
-    E::Int64
-    Alist::Vector{Vector{Any}}
-    A::Array{Float64}
-    C::Array{Float64}
-    G::Array{Float64}
-    Z::Array{Float64}
-    B0::Float64
-    Q0::Float64
-    anchor_list::Array{Float64}
-    Dim::Vector{Float64}
-end
 
-struct MapDef
-    Alist::Vector{Vector{Any}}
-    A::Array{Float64}
-    C::Array{Float64}
-    G::Array{Float64}
-    Z::Array{Float64}
-    anchor_list::Vector{Float64}
-    Dim::Vector{Float64} #[n.m]
-    nN::Int64 #number of houses (total N - n*m + nN)
-    GPS_locs::Array{Float64}
-    void_list::Vector{Float64}
-end
-
-struct MapProb
-    S::Int64
-    E::Int64
-    B0::Int64
-    Q0::Int64
-    Bmax::Float64
-end
-
-
-
-# 3D Structs
-#Define label:
-#(C, B, G, i, k, F, GenOnPrior)
-#In heap:
-#(F, [C, B, G, i, k, F, GenOnPrior])
-
-struct GridProb
-    S::Int64
-    E::Int64
-    GFlipped::SparseMatrixCSC{Bool, Int64}
-    B0::Float64
-    Q0::Float64
-    Bmax::Float64
-    StartCost::Float64
-    anchor_list::SparseVector{Float64,Int64}
-    Dim::Vector{Float64}
-end
-
-struct GraphDef3D
-    Alist::Vector{Vector{Any}}
-    A::SparseMatrixCSC{Float64, Int64}
-    F::SparseMatrixCSC{Float64, Int64}
-    C::SparseMatrixCSC{Float64, Int64}
-    Z::SparseMatrixCSC{Float64, Int64}
-end
-struct FullDef3D
-    S::Int64
-    E::Int64
-    Alist::Vector{Vector{Any}}
-    A::Array{Float64}
-    F::Array{Float64}
-    C::Array{Float64}
-    G::Array{Bool}
-    Z::Array{Float64}
-    B0::Float64
-    Q0::Float64
-    Bmax::Float64
-    StartCost::Float64
-    anchor_list::Array{Float64}
-    Dim::Vector{Float64}
-end
-struct MapDef3D
-    Alist::Vector{Vector{Any}}
-    A::Array{Float64}
-    C::Array{Float64}
-    F::Array{Float64}
-    G::Array{Float64}
-    Z::Array{Float64}
-    anchor_list::Vector{Float64}
-    Dim::Vector{Float64} #[n.m]
-    nN::Int64 #number of "houses" (total N - n*m + nN)
-    GPS_locs::Array{Float64}
-    void_list::Vector{Float64}
-end
-struct EucGraph
-    S::Int64
-    E::Int64
-    Alist::Vector{Vector{Any}}
-    F::SparseMatrixCSC{Bool, Int64}
-    C::SparseMatrixCSC{Float64, Int64}
-    GFlipped::SparseMatrixCSC{Bool, Int64}
-    Z::SparseMatrixCSC{Float64, Int64}
-    B0::Float64
-    Q0::Float64
-    Bmax::Float64
-    StartCost::Float64
-    anchor_list::SparseVector{Int64}
-    locs::Matrix{Float64}
-end
-struct EucGraphInt
-    S::Int64
-    E::Int64
-    Alist::Vector{Vector{Any}}
-    F::SparseMatrixCSC{Bool, Int64}
-    C::SparseMatrixCSC{Int64}
-    GFlipped::SparseMatrixCSC{Bool, Int64}
-    Z::SparseMatrixCSC{Int64}
-    B0::Int64
-    Q0::Int64
-    Bmax::Int64
-    StartCost::Int64
-    anchor_list::SparseVector{Int64}
-    locs::Matrix{Float64} #leave this as float... need to deal with herustic cost.... can just round down?
-end
-struct Battery{N<:AbstractFloat, F<:Function}
-    R::N
-    Cmax::N
-    C::N
-    OCV::F
-end
 
 
 ###########################################
