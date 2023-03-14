b37abf73363802fefdb0da0a5073bb2810a01afd
drew <you@example.com>
2023-03-14 12:28:29 -0400

    changed nidx and NVec...

--- src/solve_functions.jl
+++ src/solve_functions.jl
@@ -13,7 +13,8 @@ function solve_euc(;algo::String = "label", dims::String="2D", heur::String = "a
     # Nvec = [30:40:200; 2000:100:10000; 10000:1000:10000]
     # Nvec = [50:50:2000; 3000:1000:20000]
     Nvec = [50:500:2000; 2000:1000:20000]
-    Nstart > 1900 && (Nvec = Nstart:1000:20000)
+    nidx = 0
+    Nstart > 1900 && (nidx = findall(x->x==Nstart, Nvec)[1] - 1)
 
 
     if algo == "label"
