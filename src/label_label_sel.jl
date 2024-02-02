078b152551a02457bd816614c86b8833cc967428
drew <you@example.com>
2024-02-02 12:09:39 -0500

    rework with label structs...

--- src/label_label_sel.jl
+++ src/label_label_sel.jl
@@ -1,18 +1,146 @@
-#we want to wrtie a labeling algo which gets SPP from current node to goal as lower bound....
-# "expensive"  to compute, but will this reduce the search space enough to make it worth it?
-#as we go along, can we re-use Astar computations as we evaluate new nodes?.
+function hybrid_label_selection(def::EucGraphInt; heur::String="astar")
+    start, goal, Alist, F, C, Z = def.S, def.E, def.Alist, def.F, def.C, def.Z
+    Bstart, Qstart = def.B0, def.Q0
+    Cmin = minimum(nonzeros(C))
+    genpen = Cmin * 0
+    Bstart = Int(floor(5 * mean(nonzeros(C))))
+    Qstart = 9999
+    Bmax = Bstart
+    SC = def.StartCost
+    SC = 0
+    anchor = def.anchor_list  #if a grid problem, this is zeros(N)
+    locs = def.locs
+    Dim = def.locs
 
-#Opt path from S -> E (SPP) also contains optimal path from every node in that path (i,j,k) -> e
-#so longer SPP computatiosn (farther from goal) will give us more info, can reuse.
-# using ProgressMeter
+    N = length(Alist)
+    graph = make_graph(def, false)
+    Fvec = fill(2^63 - 1, N)
+    heur_astar = get_heur_astar(goal, locs, Fvec)
+    heur_astar(start)
+    if heur == "astar"
+        heur_label! = get_heur_label(Fvec, graph, C, goal, heur_astar)
+    elseif heur == "euc"
+        heur_label! = get_heur_label_euc(Fvec, locs, goal)
+    else
+        println("invalid heuristic... breaking")
+        return return 0, [0], Bool.([0])
+    end
+    if heur_label!(start) == 2^63 - 1
+        return -1, [0], Bool.([0])
+    end
+
+    label_init = MyLabel(
+        batt_state=Bstart,
+        gen_state=Qstart,
+        gcost=0,
+        fcost=heur_label!(start),
+        hcost=heur_label!(start),
+        node_idx=start,
+        prior_node_idx=0,
+        _hold_came_from_prior=0,
+        came_from_idx=1,
+        pathlength=1,
+        _hold_gen_track_prior=0,
+        gentrack_idx=1,
+        gen_bool=0,
+    )
+    Q = MutableBinaryMinHeap{MyLabel}()
+    push!(Q, label_init)
+
+    P = [MyLabel[] for k = 1:N] #tuple of vectors of labels.... may change to hashmap???????? or set.... 
+
+    came_from = [Tuple{Int64, Int64}[] for _ in 1:N]
+    push!(came_from[start], (0, 9999)) #S is start... so we just add a dummy entry to [S]
+    gen_track = [Tuple{Int64,Int64}[] for _ in 1:N]  #data struct to track genertor patterns 
+    z = 0  #will not have a path larger than N
+    while true 
+        isempty(Q) && (printstyled("Q empty, Z  = $z... \n", color=:light_cyan); break)
+
+        #pull minimum cost label....
+        labelN = pop!(Q)
+        nodei = labelN.node_idx
+        GenPrior = 1 #label_treated[7]
+        # P[nodei] = vcat(P[nodei], reshape(label_treated, (1, 9)))
+        push!(P[nodei], labelN)
+
+        if nodei == goal
+            opt_cost = labelN.gcost
+            opt_path = get_path(labelN, came_from, start)
+            opt_gen = get_gen(labelN, gen_track)
+            return opt_cost, opt_path, opt_gen
+        end
+        pathi = get_path(labelN, came_from, start)
+        for nodej in Alist[nodei]
+            nodej == nodei && continue
+            nodej ∈ pathi && continue
+            
+            hj = heur_label!(nodej)
+            Fbin = F[nodei, nodej] # if we can glide, then ALWAYS glide
+
+            #GEN ON
+            new_batt_state = labelN.batt_state - C[nodei, nodej] * (1 - Fbin) + Z[nodei, nodej] - SC * (1 - GenPrior)
+            new_gen_state = labelN.gen_state - Z[nodei, nodej]
+            Gij = def.GFlipped[nodei, nodej]
+            if Gij == 0 && new_batt_state >= 0 && new_gen_state >= 0 && new_batt_state <= Bmax
+                temp_new_label = MyLabel(
+                    gcost = labelN.gcost + C[nodei, nodej],
+                    fcost = labelN.gcost + C[nodei, nodej] + hj,
+                    hcost = hj,
+                    node_idx = nodej,
+                    prior_node_idx = labelN.node_idx,
+                    _hold_came_from_prior = labelN.came_from_idx,
+                    came_from_idx = -1, #fill in later! (in update func)
+                    pathlength = labelN.pathlength + 1,
+                    _hold_gen_track_prior = labelN.gentrack_idx,
+                    gentrack_idx = -1,  #fill in later!
+                    gen_bool = 1,
+                    batt_state = new_batt_state,
+                    gen_state =  new_gen_state
+                )
+                if EFF_heap(Q, temp_new_label) && EFF_P(P, temp_new_label)
+                    new_label = update_path_and_gen!(temp_new_label, came_from, gen_track)
+                    push!(Q, new_label)
+                end
+            end
+
+            #GEN OFF
+            new_batt_state = labelN.batt_state - C[nodei, nodej] * (1 - Fbin)
+            if new_batt_state >= 0 
+                temp_new_label = MyLabel(
+                    gcost = labelN.gcost + C[nodei, nodej],
+                    fcost = labelN.gcost + C[nodei, nodej] + hj,
+                    hcost = hj,
+                    node_idx = nodej,
+                    prior_node_idx = labelN.node_idx,
+                    _hold_came_from_prior = labelN.came_from_idx,
+                    came_from_idx = -1, #fill in later! (in update func)
+                    pathlength = labelN.pathlength + 1,
+                    _hold_gen_track_prior = labelN.gentrack_idx,
+                    gentrack_idx = -1,  #fill in later!
+                    gen_bool = 0,
+                    batt_state = new_batt_state,
+                    gen_state = labelN.gen_state
+                )
+
+                if EFF_heap(Q, temp_new_label) && EFF_P(P, temp_new_label)
+                    new_label = update_path_and_gen!(temp_new_label, came_from, gen_track)
+                    push!(Q, new_label)
+                end
+            end
+        end
+        z += 1
+        z == 200_000 && (printstyled("ZBREAK@$(z)", color=:light_red); break)
+    end
+    return 0, [0], Bool.([0])
+end
 
 
-##############################################
+
 """
     input EucGraphInt instance
     heur - optional argument: "euc" or "astar" (manhattan not working right now)
 """
-function hybrid_label_selection(def::EucGraphInt; heur::String = "astar") 
+function hybrid_label_selection_dumb(def::EucGraphInt; heur::String = "astar") 
     S, E, Alist, F, C, Z = def.S, def.E, def.Alist, def.F, def.C, def.Z
     Bstart, Qstart = def.B0, def.Q0
     Cmin = minimum(nonzeros(C))
@@ -168,153 +296,3 @@ function hybrid_label_selection(def::EucGraphInt; heur::String = "astar")
     end
     return 0,[0], Bool.([0])
 end
-
-
-##
-# @load "Problems\\euc_probs_disc\\550_4conn_9" euc_inst
-# @load "Problems\\lattice_probs_2D\\50_1" lattice_inst
-# @time hybrid_label_selection(euc_inst)
-
-##
-# @load "Problems\\lattic/e_probs\\20_1" lattice_inst
-# @load "Problems\\euc_probs\\50_4conn_1" euc_inst
-# @time costL, pathL, genL, lX, look, dist = hybrid_with_LB(lattice_inst, heur = "astar")
-# set_default_plot_size(15cm, 10cm)
-# plot(x = 1:length(dist), y = dist, Geom.line, Guide.xlabel("Iterations"), Guide.ylabel("Distance to Goal"))
-# Alist = euc_inst.Alist
-# N = length(Alist)
-# # println("Solving with $(N) Nodes || B0 = $(Bstart)")
-# graph = make_graph(def)
-# Fvec = fill(NaN, N)
-# heur_astar = get_heur_astar(E, locs, Fvec)
-# heur_astar(euc_inst.S)
-# heur_label! = get_heur_label(Fvec, graph, C, E, heur_astar)
-# heur_label!(euc_inst.S)
-# ##
-# def = euc_inst
-# S, E, Alist, F, C, Z = def.S, def.E, def.Alist, def.F, def.C, def.Z
-# Bstart, Qstart = def.B0, def.Q0
-# Cmin = minimum(nonzeros(C))
-# # Bstart = mean(nonzeros(C)) * 4
-# genpen = Cmin*.0
-# Bstart = 9999+mean(nonzeros(C)) + 5*maximum(C)
-# Qstart = 9999
-# Bmax = Bstart
-# SC = def.StartCost
-# SC = 0
-# anchor = def.anchor_list  #if a grid problem, this is zeros(N)
-# locs = def.locs
-# # G = .!def.GFlipped
-# Dim = def.locs           ####### pass third argument of heuristics as locs
-
-# N = length(Alist)
-# # println("Solving with $(N) Nodes || B0 = $(Bstart)")
-# graph = make_graph(def)
-# Fvec = fill(NaN, N)
-# heur_astar = get_heur_astar(E, locs, Fvec)
-# heur_astar(S)
-# heur_label! = get_heur_label(Fvec, graph, C, E, heur_astar)
-# if heur_label!(S) == Inf     
-#     println("Infinite Start Heuristic...")
-#     return 0,0,0,0,0,0,0
-# end
-##now test labeling algo with astar LB.....
-
-# @load "Problems\\euc_probs\\1990_8conn" euc_inst
-# @time out = hybrid_with_LB(euc_inst)
-# println("Labels made: $(out[end])")
-
-
-
-# @load "Problems\\euc_probs2D\\50_4conn_1" euc_inst
-# cost, path, gen, lX, look = hybrid_with_LB(euc_inst)
-# @load "Solutions\\euc_probs2D\\50_4conn_1" tdp cost path gen lX
-# pathA1 = a_star(graph, euc_inst.S, euc_inst.E, euc_inst.C)
-# pathA = [pathA1[i].src for i in 1:length(pathA1)]
-# append!(pathA, pathA1[end].dst)
-# plot_euc_graph(euc_inst, path = path, gen = gen)
-# plot_euc_graph(euc_inst, path = pathA, gen = gen)
-
-
-
-
-######################################################################################
-
-
-## first do an Astar test with Graphs.jl package
-# @load "Problems\\euc_probs\\30_4conn" euc_inst
-
-# Alist = euc_inst.Alist
-# S, E = euc_inst.S, euc_inst.E
-# C= euc_inst.C
-# locs = euc_inst.locs
-# Fvec = fill(NaN, length(Alist))
-# plt = plot_euc_graph(euc_inst)
-# heur_astar = get_heur_astar(E, locs, Fvec)
-# graph = make_graph(euc_inst)
-# plot_euc_graph(euc_inst)
-
-# @time path_astar = a_star(graph, S, E, C, heur_astar)
-
-# path_list, LB_vec, cost = astar_proc(path_astar, C)
-# println("from $(S) to $(E)")
-# println("cost: $(cost)")
-# display(path_list)
-
-# ##
-# Fvec = fill(NaN, length(Alist))
-# heur_astar = get_heur_astar(E, locs, Fvec)
-# heur_label! = get_heur_label(Fvec, graph, C, E, heur_astar)
-# heur_label!(euc_inst.S)
-
-
-# ## time getting SPP for every node...
-# function get_total_heur_time(euc_inst)
-#     Fvec = fill(NaN, length(euc_inst.Alist))
-#     heur_astar = get_heur_astar(euc_inst.E, euc_inst.locs, Fvec); heur_astar(euc_inst.S)
-#     graph = make_graph(euc_inst)
-#     heur_label! = get_heur_label(Fvec, graph, euc_inst.C, euc_inst.E, heur_astar)
-#     heur_label!(1)
-#     total_time = 0
-#     for i in 1:length(euc_inst.Alist)
-#         tdp = @elapsed h = heur_label!(i)
-#         total_time += tdp
-        
-#     end
-#     return total_time
-# end
-
-
-# Nvec = 30:40:2000
-# tvec4 = zeros(length(Nvec))
-# tvec8 = zeros(length(Nvec))
-
-# cnt = 0
-# for n in Nvec
-#     println(n)
-#     cnt += 1
-    
-#     conn = 4
-#     @load "Problems\\euc_probs\\$(n)_$(conn)conn" euc_inst
-#     t = get_total_heur_time(euc_inst)
-#     tvec4[cnt] = t
-
-#     conn = 8
-#     @load "Problems\\euc_probs\\$(n)_$(conn)conn" euc_inst
-#     t = get_total_heur_time(euc_inst)
-#     tvec8[cnt] = t
-    
-# end
-# plt = Gadfly.plot(
-#     layer(x = Nvec, y = tvec4, Geom.line, Geom.point, Theme(default_color = "grey")),
-#     layer(x = Nvec, y = tvec8, Geom.line, Geom.point, Theme(default_color = "red")),
-#     Guide.manual_color_key("", ["branch = 4", "branch = 8"], ["grey", "red"]),
-#     Guide.title("Time to get A-star LB for every Node in Graph"),
-#     Guide.xlabel("Numer of Nodes"),
-#     Guide.ylabel("Time (s)")
-#            )
-
-# ##  PLot the graph....
-# graph = make_graph(euc_inst)
-# plot_euc_graph(euc_inst)
-
